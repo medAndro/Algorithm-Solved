cca91c2f23cc25b456864f616a67d40d6fe3faea
Meda <medAndro@protonmail.com>
2024-01-18 02:44:06 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 456 ms, Memory: 58044 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,8 @@
+import sys
+N = int(sys.stdin.readline())
+arr = []
+for i in range(N):
+    arr.append(list(map(int,sys.stdin.readline().split())))
+arr.sort(key=lambda x:(x[0],x[1]))
+for i in arr:
+    sys.stdout.writelines(str(i[0]) +" " +str(i[1])+"\n")
