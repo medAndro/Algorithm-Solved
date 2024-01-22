4d6eaeb7a6914ad391df61b0ff4dc492c351238b
Meda <medAndro@protonmail.com>
2024-01-23 07:56:57 +0900

    [Silver III] Title: N과 M (3), Time: 1336 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,5 +1,13 @@
-import itertools as its
 N, M = map(int, input().split())
+ans = []
+def backTrack(depth):
+    global ans, N, M
+    if depth == M:
+        print(" ".join(map(str, ans)))
+        return
 
-for i in list(its.product(list(range(1,N+1)), repeat=M)):
-    print(" ".join(map(str, i)))
+    for i in range(1, N+1):
+        ans.append(i)
+        backTrack(depth+1)
+        ans.pop()
+backTrack(0)
