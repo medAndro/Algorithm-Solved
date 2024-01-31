82004807578742250803dea63661189f4ac2ec04
Meda <medAndro@protonmail.com>
2024-01-31 16:37:26 +0900

    [Silver II] Title: 부분수열의 합, Time: 172 ms, Memory: 111952 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,9 +1,15 @@
-import itertools as its
 N, S = map(int, input().split())
 arr = list(map(int, input().split()))
 answer = 0
-for j in range(1, N+1):
-    for i in list(its.combinations(arr, j)):
-        if sum(i) == S:
-            answer += 1
+def dfs(idx, s):
+    global answer
+    if idx >= N:
+        return
+    s+= arr[idx]
+    if s == S:
+        answer+=1
+    dfs(idx+1, s-arr[idx])
+    dfs(idx+1, s)
+
+dfs(0,0)
 print(answer)
