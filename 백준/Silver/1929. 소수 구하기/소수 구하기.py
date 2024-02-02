11b3abd655e6104f74b5da591e561182d3ed2715
Meda <medAndro@protonmail.com>
2024-02-02 13:26:39 +0900

    [Silver III] Title: 소수 구하기, Time: 172 ms, Memory: 119188 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,26 +1,19 @@
-import sys
 M, N = map(int, input().split())
-primes = [1]*1000001
-primes[0] = 0
-primes[1] = 0
-for i in range(2, 1000):
-    if primes[i] == 1:
-        isprime = True
-        if i >= 3:
-            for j in range(2,i-1):
-                if i%j == 0:
-                    isprime = False
-        c = 1
-        if isprime:
-            while True:
-                try:
-                    c += 1
-                    primes[i*c] = 0
-                except:
-                    break
-        else:
-            primes[i] = 0
+eratosthenes = [1] * 1000001
+eratosthenes[1] = 0
+for i in range(2, 1001):
+    isprime = True
+    if i >= 3:
+        for j in range(2, i):
+            if i % j == 0:
+                isprime = False
+                break
+    if isprime:
+        ii = 2
+        while i * ii <= 1000000:
+            eratosthenes[i * ii] = 0
+            ii += 1
 
 for i in range(M, N+1):
-    if primes[i]==1:
-        sys.stdout.writelines(str(i)+"\n")
+    if eratosthenes[i]:
+        print(i)
