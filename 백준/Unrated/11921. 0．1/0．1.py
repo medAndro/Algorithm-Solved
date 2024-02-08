6e53174aafb40277ff6fe6656bb4518d5cc7ef18
Meda <medAndro@protonmail.com>
2024-02-09 07:38:53 +0900

    [Unrated] Title: 0.1, Time: 44 ms, Memory: 31120 KB, Score: 10000 point -BaekjoonHub

--- 백준/Unrated/11921. 0．1/0．1.py
+++ 백준/Unrated/11921. 0．1/0．1.py
@@ -1,7 +1,7 @@
 import sys
 sys.stdin.readline()
 answer = 0
-for _ in range(5):
+for _ in range(10000):
     answer+=int(sys.stdin.readline())
-sys.stdout.write("5"+'\n')
+sys.stdout.write("10000"+'\n')
 sys.stdout.write(str(answer))
