6685633a10f66fc1ddc82a2f4dd0accf25819056
Meda <medAndro@protonmail.com>
2024-02-20 11:51:57 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 3348 ms, Memory: 110616 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,27 +1,19 @@
 N, K = map(int, input().split())
-LL = {}
-AnsArr = []
-for i in range(1,N+1):
-    if i == N:
-        LL[i] = 1
-    else:
-        LL[i] = i + 1
-cur = 1
-i = 0
-prev = 1
-while len(LL)>0:
-    i += 1
-    if i == K:
-        next = LL.get(cur)
-        AnsArr.append(cur)
-        LL.pop(cur)
-        if prev in LL:
-            LL[prev] = next
-        cur = next
-        i = 0
-    else:
-        prev = cur
-        cur = LL.get(cur)
-print("<",end="")
-print(*AnsArr, sep=", ", end="")
-print(">")
+answer = []
+arr = list(range(1,N+1))
+
+idx = 0
+cnt = 1
+while len(answer) < N:
+    if idx == N:
+        idx = 0
+    if arr[idx] != 0:
+        if cnt > K:
+            cnt = 1
+        if cnt == K:
+            answer.append(arr[idx])
+            arr[idx] = 0
+        cnt += 1
+    idx+=1
+
+print("<"+", ".join(map(str, answer))+">")
