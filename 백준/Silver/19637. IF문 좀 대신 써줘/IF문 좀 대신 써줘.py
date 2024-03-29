c718096cae4cbcdbf75fcd6a6ef6041889c6a4c3
Meda <medAndro@protonmail.com>
2024-03-29 14:00:50 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 244 ms, Memory: 116536 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
@@ -2,14 +2,13 @@ import sys
 import bisect
 
 N, M = map(int, sys.stdin.readline().split())
-title = []
-power = []
+title = ['']*N
+power = [0]*N
 
-inputs = sys.stdin.readlines()
 for i in range(N):
-    t, val = inputs[i].rstrip().split()
-    title.append(t)
-    power.append(int(val))
-
+    t, val = sys.stdin.readline().split()
+    title[i] = t
+    power[i] = int(val)
 for i in range(M):
-    print(title[bisect.bisect_left(power, int(inputs[i+N]))])
+    p = int(sys.stdin.readline())
+    print(title[bisect.bisect_left(power, p)])
