09334b8ee400b4bc217919331706fab6de5071e7
Meda <medAndro@protonmail.com>
2024-04-16 06:50:07 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,14 +1,15 @@
+import sys
+input = sys.stdin.readline
 N = int(input())
 rgb = []
 for _ in range(N):
-    rgb.append(tuple(map(int, input().split())))
+    rgb.append(list(map(int, input().split())))
 
-currentRGB = list(rgb[0])
 for i in range(1, N):
     r, g, b = rgb[i]
-    r += min(currentRGB[1], currentRGB[2])
-    g += min(currentRGB[0], currentRGB[2])
-    b += min(currentRGB[0], currentRGB[1])
-    currentRGB = [r,g,b]
+    r += min(rgb[0][1], rgb[0][2])
+    g += min(rgb[0][0], rgb[0][2])
+    b += min(rgb[0][0], rgb[0][1])
+    rgb[0] = [r,g,b]
 
-print(min(currentRGB))
+print(min(rgb[0]))
