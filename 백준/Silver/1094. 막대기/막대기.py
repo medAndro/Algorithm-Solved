00b8337f0abc528290b89d7a6acad2f88e00cc9f
Meda <medAndro@protonmail.com>
2024-04-24 07:00:57 +0900

    [Silver V] Title: 막대기, Time: 104 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.py
+++ 백준/Silver/1094. 막대기/막대기.py
@@ -1,15 +1,1 @@
-X = int(input())
-
-sticks = [64]
-
-while sum(sticks) > X:
-    minStick = min(sticks)
-    sticks.remove(minStick)
-
-    if (sum(sticks) + minStick // 2) >= X:
-        sticks.append(minStick // 2)
-    else:
-        sticks.append(minStick // 2)
-        sticks.append(minStick // 2)
-
-print(len(sticks))
+print(sum(map(int,list(str(bin( int(input())))[2:]))))
