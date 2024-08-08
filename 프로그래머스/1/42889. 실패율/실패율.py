01de3c330cc5c9ffa9027b64c2c66895bc2e97e9
Meda <medAndro@protonmail.com>
2024-08-08 09:50:47 +0900

    [level 1] Title: 실패율, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,14 +1,12 @@
 def solution(N, stages):
-    sList = [0] * (N+1)
+    sList = [0] * (N + 1)
     for s in stages:
         sList[s - 1] += 1
     sumList = sList.copy()
     for i in range(N - 1, -1, -1):
         sumList[i] += sumList[i + 1]
+
     ratio = list(map(lambda x, y: x / y if y != 0 else 0, sList[0:N], sumList[0:N]))
-    r = list(zip(ratio, list(range(1, N + 1))))
-    r.sort(key=lambda x: (-x[0], x[1]))
-    answer = [0] * N
-    for i in range(N):
-        answer[i] = r[i][1]
+    r = dict(zip(list(range(1, N + 1)), ratio))
+    answer = sorted(r, key=lambda x: r[x], reverse=True)
     return answer
