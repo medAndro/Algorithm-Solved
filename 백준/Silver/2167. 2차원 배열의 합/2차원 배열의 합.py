b1a2b71022637ca6b13f6a467f418c39440ed20b
Meda <medAndro@protonmail.com>
2024-11-15 03:09:26 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 2620 ms, Memory: 112560 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -0,0 +1,12 @@
+N, M = map(int, input().split())
+numArray = [list(map(int, input().split())) for _ in range(N)]
+
+K = int(input())
+for _ in range(K):
+    answer = 0
+    i,j,x,y = map(lambda n: n-1, map(int, input().split()))
+
+    for row in range(i, x+1):
+        answer += sum(numArray[row][j:y+1])
+
+    print(answer)
