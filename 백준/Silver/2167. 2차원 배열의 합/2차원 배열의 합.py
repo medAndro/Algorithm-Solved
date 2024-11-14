c6f5e8ffd4855a2dbc8c0406ae69f8b5f2b53dfb
Meda <medAndro@protonmail.com>
2024-11-15 03:44:55 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 124 ms, Memory: 112036 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -1,15 +1,27 @@
+from itertools import accumulate
+from operator import add
 import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-numArray = [list(map(int, input().split())) for _ in range(N)]
+rowPrefixSum = [list(accumulate(list(map(int, input().split())))) for _ in range(N)]
+prefixSum = []
+for i in range(N):
+    if i==0:
+        prefixSum.append(rowPrefixSum[i])
+        continue
+    else:
+        prefixSum.append(list(map(add, prefixSum[i-1], rowPrefixSum[i])))
 
-K = int(input())
-for _ in range(K):
-    answer = 0
-    i,j,x,y = map(lambda n: n-1, map(int, input().split()))
+T = int(input())
+for _ in range(T):
+    i,j,x,y = map(lambda n : n - 1, map(int, input().split()))
+    answer = prefixSum[x][y]
 
-    for row in range(i, x+1):
-        answer += sum(numArray[row][j:y+1])
-
+    if i>0:
+        answer -= prefixSum[i-1][y]
+    if j>0:
+        answer -= prefixSum[x][j-1]
+    if i>0 and j>0:
+        answer += prefixSum[i-1][j-1]
     print(answer)
