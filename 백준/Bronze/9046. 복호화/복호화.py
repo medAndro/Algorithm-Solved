472b8cb137302eb2e859f89795990f778e25ac06
Meda <medAndro@protonmail.com>
2024-11-14 23:48:00 +0900

    [Bronze II] Title: 복호화, Time: 196 ms, Memory: 119632 KB -BaekjoonHub

---
+++ 백준/Bronze/9046. 복호화/복호화.py
@@ -0,0 +1,21 @@
+import string
+
+T = int(input())
+
+for _ in range(T):
+    text = input()
+
+    alphaDict = dict(zip(string.ascii_lowercase, [0]*26))
+
+    for char in text:
+        if char == ' ':
+            continue
+        alphaDict[char] += 1
+
+    alphaList = list(zip(alphaDict.values(), alphaDict.keys()))
+    alphaList.sort(reverse=True)
+
+    if alphaList[0][0] == alphaList[1][0]:
+        print('?')
+    else:
+        print(alphaList[0][1])
