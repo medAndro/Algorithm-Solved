5e66721f400354307b3eda61def00e0cb401f01e
Meda <med4ndro@gmail.com>
2026-04-20 00:01:09 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 376.29 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -1,47 +1,53 @@
+from collections import deque
+
 def rotate(board, point):
     row1, col1 = point[0] - 1, point[1] - 1
     row2, col2 = point[2] - 1, point[3] - 1
     horizontal_len = abs(col2 - col1 + 1)
     vertical_len = abs(row2 - row1 + 1)
-    perimeter = horizontal_len * 2 + vertical_len * 2 - 4
 
+    min_val = board[row1][col1]
     r = row1
     c = col1 - 1
-    arr = []
+    deq = deque()
     for _ in range(horizontal_len):
         c += 1
-        arr.append(board[r][c])
+        deq.append(board[r][c])
+        if board[r][c] < min_val: min_val = board[r][c]
     for _ in range(vertical_len - 1):
         r += 1
-        arr.append(board[r][c])
+        deq.append(board[r][c])
+        if board[r][c] < min_val: min_val = board[r][c]
     for _ in range(horizontal_len - 1):
         c -= 1
-        arr.append(board[r][c])
+        deq.append(board[r][c])
+        if board[r][c] < min_val: min_val = board[r][c]
     for _ in range(vertical_len - 2):
         r -= 1
-        arr.append(board[r][c])
-    arr = [arr[-1]] + arr[0:-1]
+        deq.append(board[r][c])
+        if board[r][c] < min_val: min_val = board[r][c]
+    deq.rotate(1)
 
     r = row1
     c = col1 - 1
     index = 0
     for _ in range(horizontal_len):
         c += 1
-        board[r][c] = arr[index]
+        board[r][c] = deq[index]
         index += 1
     for _ in range(vertical_len - 1):
         r += 1
-        board[r][c] = arr[index]
+        board[r][c] = deq[index]
         index += 1
     for _ in range(horizontal_len - 1):
         c -= 1
-        board[r][c] = arr[index]
+        board[r][c] = deq[index]
         index += 1
     for _ in range(vertical_len - 2):
         r -= 1
-        board[r][c] = arr[index]
+        board[r][c] = deq[index]
         index += 1
-    return min(arr)
+    return min_val
 
 
 def solution(rows, columns, queries):
