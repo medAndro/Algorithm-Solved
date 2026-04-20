9c6348b4359c7742b7429e2bb83ebbb4d7eb1e2b
Meda <med4ndro@gmail.com>
2026-04-21 06:25:56 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.04 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,37 +1,44 @@
-room_size = 5
-
-
-def recursive_pos_check(board, visited, pos, depth):
-    if depth == 2:
-        return True
-    visited.append(pos)
-    dr = [-1, 1, 0, 0]
-    dc = [0, 0, -1, 1]
-
-    result = True
-    for index in range(4):
-        pos_r = pos[0] + dr[index]
-        pos_c = pos[1] + dc[index]
-        if pos_r < 0 or pos_c < 0 or pos_r == room_size or pos_c == room_size or board[pos_r][pos_c] == 'X':
-            continue
-        if [pos_r, pos_c] in visited:
-            continue
-        elif board[pos_r][pos_c] == 'P':
-            return False
-        elif board[pos_r][pos_c] == 'O':
-            if not recursive_pos_check(board, visited.copy(), [pos_r, pos_c], depth + 1):
-                return False
-    return result
-
-
 def room_checker(room):
-    for r in range(room_size):
-        for c in range(room_size):
+    for r in range(len(room)):
+        for c in range(len(room)):
             if room[r][c] == 'P':
-                if not recursive_pos_check(room, [], [r, c], 0):
-                    return 0
-                else:
-                    continue
+                isCanMoveDownOne = r != 4
+                isCanMoveDownTwo = r < 3
+                isCanMoveRightOne = c != 4
+                isCanMoveRightTwo = c < 3
+                isCanMoveLeftOne = c > 0
+
+                # 밑에 사람이 있는 경우
+                if isCanMoveDownOne:
+                    if room[r + 1][c] == 'P':
+                        return 0
+
+                # 오른쪽에 사람이 있는 경우
+                if isCanMoveRightOne:
+                    if room[r][c + 1] == 'P':
+                        return 0
+
+                # 오른쪽 또는 밑이 파티션이 아닌데 오른쪽 아래에 사람이 있는 경우
+                if isCanMoveRightOne and isCanMoveDownOne:
+                    if room[r][c + 1] == 'O' or room[r + 1][c] == 'O':
+                        if room[r + 1][c + 1] == 'P':
+                            return 0
+
+                # 왼쪽 또는 밑이 파티션이 아닌데 왼쪽 아래에 사람이 있는 경우
+                if isCanMoveLeftOne and isCanMoveDownOne:
+                    if room[r][c - 1] == 'O' or room[r + 1][c] == 'O':
+                        if room[r + 1][c - 1] == 'P':
+                            return 0
+
+                #  밑이 파티션이 아닌데 밑의 밑에 사람이 있는 경우
+                if isCanMoveDownTwo and room[r + 1][c] == 'O':
+                    if room[r + 2][c] == 'P':
+                        return 0
+
+                #  오른쪽이 파티션이 아닌데 오른쪽의 오른쪽에 사람이 있는 경우
+                if isCanMoveRightTwo and room[r][c + 1] == 'O':
+                    if room[r][c + 2] == 'P':
+                        return 0
     return 1
 
 
