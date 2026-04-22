f8e5f6b9db4151398334d8d82779b458d82171f0
Meda <med4ndro@gmail.com>
2026-04-22 17:03:17 +0900

    [level 1] Title: 시저 암호, Time: 1.90 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,20 +1,10 @@
 def solution(s, n):
-    answer = ['a'] * len(s)
+    answer = [' '] * len(s)
 
     for i in range(len(s)):
         if s[i] == ' ':
-            answer[i] = ' '
-        elif ord(s[i]) in range(ord('A'), (ord('Z') + 1)):
-            new_ord = ord(s[i]) + n
-            if new_ord > ord('Z'):
-                answer[i] = chr(new_ord - ord('Z') + ord('A') - 1)
-            else:
-                answer[i] = chr(new_ord)
-        elif ord(s[i]) in range(ord('a'), (ord('z') + 1)):
-            new_ord = ord(s[i]) + n
-            if new_ord > ord('z'):
-                answer[i] = chr(new_ord - ord('z') + ord('a') - 1)
-            else:
-                answer[i] = chr(new_ord)
+            continue
+        base = ord('A') if s[i].isupper() else ord('a')
+        answer[i] = chr((ord(s[i]) - base + n) % 26 + base)
 
     return ''.join(answer)
