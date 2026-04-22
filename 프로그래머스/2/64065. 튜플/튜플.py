5292388b1a6090cb32942cd3a2f2a4e97a7df9ba
Meda <med4ndro@gmail.com>
2026-04-23 02:12:21 +0900

    [level 2] Title: 튜플, Time: 14.75 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,14 +1,11 @@
 def solution(s):
     strs_lists = list(map(lambda x: x.split(','), s[2:-2].split('},{')))
     strs_lists.sort(key=len)
-    answer = [int(strs_lists[0][0])]
-    if len(strs_lists) == 1:
-        return answer
-
-    for i in range(1, len(strs_lists)):
-        for c in strs_lists[i]:
-            if c not in strs_lists[i - 1]:
-                answer.append(int(c))
+    answer = dict()
+    for strs in strs_lists:
+        for c in strs:
+            if c not in answer:
+                answer[c] = 1
                 break
 
-    return answer
+    return list(map(int, answer))
