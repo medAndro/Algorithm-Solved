45d0b6e9a536052c9a8067bddc6d819b177203a0
Meda <med4ndro@gmail.com>
2026-04-27 06:45:45 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.71 ms, Memory: 9.46 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -0,0 +1,17 @@
+import re
+
+
+def solution(files):
+    files_for_sort = []
+    pattern = re.compile(r'(\D+)(\d{1,5})')
+    for file in files:
+        group = re.match(pattern, file)
+        head = group[1]
+        number = group[2]
+        files_for_sort.append([file, head.lower(), ("0" * 4 + number)[-5:]])
+
+    files_for_sort.sort(key=lambda x: (x[1], x[2]))
+    answer = []
+    for file in files_for_sort:
+        answer.append(file[0])
+    return answer
