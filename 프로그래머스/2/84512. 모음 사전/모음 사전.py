ffe8d3bef2c0da9fd5fe8d3621536801ee795c24
Meda <med4ndro@gmail.com>
2026-04-30 01:34:15 +0900

    [level 2] Title: 모음 사전, Time: 8.70 ms, Memory: 12.7 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,23 +1,3 @@
-import sys
-
-sys.setrecursionlimit(10 ** 6)
-
-vowel = ["A", "E", "I", "O", "U"]
-def aeiou(idx, chars, word):
-    if len(word) == len(chars) and "".join(chars) == word:
-        return idx
-
-    if not chars:
-        return aeiou(idx + 1, ['A'], word)
-    else:
-        if len(chars) < 5:
-            return aeiou(idx + 1, chars + ['A'], word)
-        else:
-            for i in range(4, -1, -1):
-                if chars[i] != "U":
-                    return aeiou(idx + 1, chars[:i] + [vowel[vowel.index(chars[i]) + 1]], word)
-            return None
-
-
 def solution(word):
-    return aeiou(0, [], word)
+    answer = 0
+    return answer
