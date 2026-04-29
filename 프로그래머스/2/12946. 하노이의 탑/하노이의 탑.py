939d016280a0041d3b2eb5086fc0bedeb5c985e9
Meda <med4ndro@gmail.com>
2026-04-29 22:56:40 +0900

    [level 2] Title: 하노이의 탑, Time: 9.03 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -1,26 +1,13 @@
+def hanoi(n, start, to, mid, answer):
+    if n == 1:
+        answer.append([start, to])
+        return
+    hanoi(n - 1, start, mid, to, answer)
+    answer.append([start, to])
+    hanoi(n - 1, mid, to, start, answer)
+
+
 def solution(n):
     answer = []
-    tower = [[i for i in range(n, 0, -1)], [], []]
-    fixed_tower_idx = 2 if n % 2 == 0 else 1
-
-    while len(tower[2]) < n:
-        ta_idx = (fixed_tower_idx + 1) % 3
-        ta = tower[ta_idx]
-        tb_idx = (fixed_tower_idx + 2) % 3
-        tb = tower[tb_idx]
-
-        if not ta:
-            ta.append(tb.pop())
-            answer.append([tb_idx + 1, ta_idx + 1])
-        elif not tb:
-            tb.append(ta.pop())
-            answer.append([ta_idx + 1, tb_idx + 1])
-        elif ta[-1] < tb[-1]:
-            tb.append(ta.pop())
-            answer.append([ta_idx + 1, tb_idx + 1])
-        elif tb[-1] < ta[-1]:
-            ta.append(tb.pop())
-            answer.append([tb_idx + 1, ta_idx + 1])
-        fixed_tower_idx = (fixed_tower_idx - 1 if n % 2 == 0 else fixed_tower_idx + 1) % 3
-
+    hanoi(n, 1, 3, 2, answer)
     return answer
