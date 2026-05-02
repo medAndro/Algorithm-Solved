9148361cd62a33f3cdde866a74c29ec986941279
Meda <med4ndro@gmail.com>
2026-05-02 18:02:07 +0900

    [level 2] Title: 소수 찾기, Time: 1024.97 ms, Memory: 9.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -14,8 +14,8 @@ def solution(numbers):
     answer = 0
     nums = set()
     for l in range(1, len(numbers) + 1):
-        for combi in permutations(numbers, l):
-            nums.add(int(''.join(list(combi))))
+        for permutation in permutations(numbers, l):
+            nums.add(int(''.join(list(permutation))))
 
     for num in nums:
         if is_prime(num):
