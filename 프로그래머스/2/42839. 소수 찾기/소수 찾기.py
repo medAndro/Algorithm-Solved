8662087ea275e73183763f0e417719f3c90e42ea
Meda <med4ndro@gmail.com>
2026-05-02 18:00:02 +0900

    [level 2] Title: 소수 찾기, Time: 1034.15 ms, Memory: 9.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,30 +1,23 @@
-import math
 from itertools import permutations
 
 
-def get_eratosthenes(max_len):
-    eratosthenes = [0, 0] + [1] * max_len
-    target = int(math.sqrt(max_len)) + 1
-
-    for i in range(2, target):
-        if eratosthenes[i] == 1:
-            cnt = 2
-            while cnt * i <= len(eratosthenes) - 1:
-                eratosthenes[cnt * i] = 0
-                cnt += 1
-    return eratosthenes
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, (num // 2) + 1):
+        if num % i == 0:
+            return False
+    return True
 
 
 def solution(numbers):
     answer = 0
-    max_numbers = int(''.join(sorted(list(numbers), reverse=True)))
     nums = set()
     for l in range(1, len(numbers) + 1):
         for combi in permutations(numbers, l):
             nums.add(int(''.join(list(combi))))
 
-    eratosthenes = get_eratosthenes(max_numbers)
     for num in nums:
-        if eratosthenes[num] == 1:
+        if is_prime(num):
             answer += 1
     return answer
