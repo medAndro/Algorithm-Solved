e63551637636303b5a91268eb1913d6b02c463f8
Meda <med4ndro@gmail.com>
2026-05-06 12:28:55 +0900

    [level 3] Title: 불량 사용자, Time: 81.13 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -3,16 +3,16 @@ import re
 
 def solution(user_id, banned_id):
     banned_regex = list(map(lambda x: x.replace('*', "."), banned_id))
-    possible_ban_users = []
-    for ban_idx in range(len(banned_id)):
-        user_idx = []
-        for idx, id in enumerate(user_id):
-            if bool(re.fullmatch(banned_regex[ban_idx], id)):
-                user_idx.append(idx)
-        possible_ban_users.append(user_idx)
-    print(possible_ban_users)
-    results = set()
 
+    possible_ban_users = []
+    for ban_idx, regex in enumerate(banned_regex):
+        ban_user_idx = []
+        for idx, id in enumerate(user_id):
+            if bool(re.fullmatch(regex, id)):
+                ban_user_idx.append(idx)
+        possible_ban_users.append(ban_user_idx)
+
+    results = set()
     def dfs(depth, current_selection):
         if depth == len(possible_ban_users):
             results.add(tuple(sorted(current_selection)))
