38b3e84814e8a19188e694732cff15b0d4115c0c
Meda <med4ndro@gmail.com>
2026-05-06 14:22:38 +0900

    [level 3] Title: 불량 사용자, Time: 86.30 ms, Memory: 19.2 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,27 +1,25 @@
 import re
+from itertools import permutations
 
 
 def solution(user_id, banned_id):
     banned_regex = list(map(lambda x: x.replace('*', "."), banned_id))
 
-    possible_ban_users = []
-    for ban_idx, regex in enumerate(banned_regex):
-        ban_user_idx = []
-        for idx, id in enumerate(user_id):
+    ban_candidates = []
+
+    for id in user_id:
+        for regex in banned_regex:
             if bool(re.fullmatch(regex, id)):
-                ban_user_idx.append(idx)
-        possible_ban_users.append(ban_user_idx)
+                ban_candidates.append(id)
+                break
 
-    results = set()
-    def dfs(depth, current_selection):
-        if depth == len(possible_ban_users):
-            results.add(tuple(sorted(current_selection)))
-            return
-        for selected_user_idx in possible_ban_users[depth]:
-            if selected_user_idx not in current_selection:
-                current_selection.append(selected_user_idx)
-                dfs(depth + 1, current_selection)
-                current_selection.pop()
+    user_permutations = list(permutations(ban_candidates, len(banned_id)))
+    user_permutations_strs = list(map(lambda x: "".join(x), user_permutations))
+    banned_full_regex = re.compile("".join(banned_regex))
 
-    dfs(0, [])
-    return len(results)
+    answer_set = set()
+    for permutation_idx, user_id_strs in enumerate(user_permutations_strs):
+        if re.fullmatch(banned_full_regex, user_id_strs):
+            answer_set.add(tuple(sorted(user_permutations[permutation_idx])))
+
+    return len(answer_set)
