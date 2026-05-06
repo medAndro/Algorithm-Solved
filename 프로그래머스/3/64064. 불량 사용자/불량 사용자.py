c5e294fbec46954a8f7300ecf0a9cb0703918be1
Meda <med4ndro@gmail.com>
2026-05-06 14:35:41 +0900

    [level 3] Title: 불량 사용자, Time: 90.64 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -4,18 +4,10 @@ from itertools import permutations
 
 def solution(user_id, banned_id):
     banned_regex = list(map(lambda x: x.replace('*', "."), banned_id))
-
-    ban_candidates = []
-
-    for id in user_id:
-        for regex in banned_regex:
-            if bool(re.fullmatch(regex, id)):
-                ban_candidates.append(id)
-                break
-
-    user_permutations = list(permutations(ban_candidates, len(banned_id)))
-    user_permutations_strs = list(map(lambda x: "".join(x), user_permutations))
-    banned_full_regex = re.compile("".join(banned_regex))
+    
+    user_permutations = list(permutations(user_id, len(banned_id)))
+    user_permutations_strs = list(map(lambda x: ",".join(x), user_permutations))
+    banned_full_regex = re.compile(",".join(banned_regex))
 
     answer_set = set()
     for permutation_idx, user_id_strs in enumerate(user_permutations_strs):
