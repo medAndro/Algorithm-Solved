e8960188d1eefaa4fda9682a6b984eabd585bc01
Meda <med4ndro@gmail.com>
2026-05-08 01:18:20 +0900

    [level 2] Title: 피로도, Time: 52.12 ms, Memory: 8.98 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,21 +1,19 @@
 def solution(k, dungeons):
     max_visited_cnt = 0
 
-    def dfs(depth, visited_mask, current_mp):
+    def dfs(visited_mask, current_mp, visit_cnt):
         nonlocal max_visited_cnt
-        if depth == len(dungeons):
-            max_visited_cnt = max(bin(visited_mask).count('1'), max_visited_cnt)
+
+        max_visited_cnt = max(visit_cnt, max_visited_cnt)
+        if visit_cnt == len(dungeons):
             return
 
-        # 현재 depth에서 k개의 던전 중 1개 던전 방문
+        # k개의 던전 중 1개 던전 방문
         for visit_idx in range(len(dungeons)):
-            # 해당 던전이 미방문한 상태일 경우
+            # 해당 던전이 미방문이면서 마나가 허용된다면 해당 던전 방문
             if not ((1 << visit_idx) & visited_mask):
                 if dungeons[visit_idx][0] <= current_mp:
-                    dfs(depth + 1, visited_mask | (1 << visit_idx), current_mp - dungeons[visit_idx][1])
+                    dfs(visited_mask | (1 << visit_idx), current_mp - dungeons[visit_idx][1], visit_cnt + 1)
 
-        # 현재 depth에서 던전 미방문
-        dfs(depth + 1, visited_mask, current_mp)
-
-    dfs(0, 0b0, k)
+    dfs(0b0, k, 0)
     return max_visited_cnt
