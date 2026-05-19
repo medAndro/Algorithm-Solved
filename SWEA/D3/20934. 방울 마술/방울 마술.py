0ea02494afaa97637b7dcf378ce241fe24e96bdf
Meda <med4ndro@gmail.com>
2026-05-19 11:58:25 +0900

    [D3] Title: 방울 마술, Time: 59 ms, Memory: 53,632 KB -BaekjoonHub

--- SWEA/D3/20934. 방울 마술/방울 마술.py
+++ SWEA/D3/20934. 방울 마술/방울 마술.py
@@ -4,14 +4,11 @@ for test_case in range(1, T + 1):
     S, K = input().split()
     K = int(K)
     S = S.index('o')
-    probability = [0.0] * 3
-    probability[S] = 1.0
+    if S == 1:
+        K += 1
+    if K == 0:
+        answer = S
+    else:
+        answer = K % 2
 
-    for _ in range(K):
-        left = probability[1] / 2
-        middle = probability[0] + probability[2]
-        right = probability[1] / 2
-        probability[0] = left
-        probability[1] = middle
-        probability[2] = right
-    print(f"#{test_case} {probability.index(max(probability))}")
+    print(f"#{test_case} {answer}")
