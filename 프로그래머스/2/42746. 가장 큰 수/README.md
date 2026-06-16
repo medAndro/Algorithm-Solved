6becb61653e5b8ca5dcf0d461c761cf8384e9e76
Meda <med4ndro@gmail.com>
2026-06-16 16:32:17 +0900

    [level 2] Title: 가장 큰 수, Time: 159.86 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 31.9 MB, 시간: 48.74 ms
+메모리: 26.3 MB, 시간: 159.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 10일 17:26:11
+2026년 06월 16일 16:32:13
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,15 +1,18 @@
-def solution(numbers):
-    numbers = list(map(str, numbers))
-    temp = []
-    for i, number in enumerate(numbers):
-        temp.append(((number * 4)[:4], numbers[i]))
-    temp.sort(key=lambda x: x[0], reverse=True)
-    answer = []
-    for number in temp:
-        answer.append(number[1])
-        
-    result = ''.join(answer)
-    if result[0] == '0':
-        return '0'
+from functools import cmp_to_key
 
-    return result
+
+def compare(a, b):
+    if a + b > b + a:
+        return -1
+    elif a + b < b + a:
+        return 1
+    else:
+        return 0
+
+
+def solution(numbers):
+    str_nums = list(map(str, numbers))
+    answer = ''.join(sorted(str_nums, key=cmp_to_key(compare)))
+    if answer[0] == '0':
+        return '0'
+    return answer
