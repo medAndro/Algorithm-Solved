40a41453a951d5051bbbcd32ad04a6d6cfbe8be0
Meda <med4ndro@gmail.com>
2026-06-16 17:35:00 +0900

    [level 2] Title: 가장 큰 수, Time: 30.30 ms, Memory: 27.6 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,18 +1,6 @@
-from functools import cmp_to_key
-
-
-def compare(a, b):
-    if a + b > b + a:
-        return -1
-    elif a + b < b + a:
-        return 1
-    else:
-        return 0
-
-
 def solution(numbers):
     str_nums = list(map(str, numbers))
-    answer = ''.join(sorted(str_nums, key=cmp_to_key(compare)))
+    answer = ''.join(sorted(str_nums, key=lambda x: x*3, reverse=True))
     if answer[0] == '0':
         return '0'
     return answer
