d7eb8a500c7f206f423180db8485cfb3dd06511a
Meda <med4ndro@gmail.com>
2026-07-27 23:20:22 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.19 ms, Memory: 57.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+	public int solution(int[][] maps) {
+		int m = maps[0].length;
+		int n = maps.length;
+
+		int answer = -1;
+		ArrayDeque<int[]> queue = new ArrayDeque<>();
+		queue.offerFirst(new int[] { 0, 0 });
+
+		int[][] dist = new int[n][m];
+		dist[0][0] = 1;
+
+		int[] dy = { -1, 0, 0, 1 };
+		int[] dx = { 0, -1, 1, 0 };
+
+		while (!queue.isEmpty() && dist[n - 1][m - 1] == 0) {
+			int[] q = queue.pollFirst();
+			int len = dist[q[0]][q[1]];
+
+			for (int i = 0; i < 4; i++) {
+				int yy = dy[i] + q[0];
+				int xx = dx[i] + q[1];
+
+				if (yy >= 0 && xx >= 0 && yy < n && xx < m) {
+					if (dist[yy][xx] == 0 && maps[yy][xx] == 1) {
+						if (yy == n - 1 && xx == m - 1) {
+							queue.clear();
+							answer = len + 1;
+							break;
+						} else {
+							queue.offerLast(new int[] { yy, xx });
+							dist[yy][xx] = len + 1;
+						}
+					}
+				}
+			}
+		}
+
+		return answer;
+	}
+}
