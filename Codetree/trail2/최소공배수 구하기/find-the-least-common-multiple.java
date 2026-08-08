f4df07b29fa76d2f55758e014213d202ed83083f
Meda <med4ndro@gmail.com>
2026-08-08 15:47:29 +0900

    [Passed] 최소공배수 구하기 (90ms, 9MB)

--- Codetree/trail2/최소공배수 구하기/find-the-least-common-multiple.java
+++ Codetree/trail2/최소공배수 구하기/find-the-least-common-multiple.java
@@ -1,29 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
-    // 유클리드 호제법을 이용한 최대공약수(GCD) 계산
-    static long gcd(long a, long b) {
-        while (b != 0) {
-            long r = a % b;
-            a = b;
-            b = r;
-        }
-        return a;
-    }
-
     // GCD를 활용한 최소공배수(LCM) 계산
-    static long lcm(long a, long b) {
-        return (a / gcd(a, b)) * b;
+    static long lcm(BigInteger n, BigInteger m) {
+        return (n.multiply(m)).divide(n.gcd(m)).longValue();
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tk = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(tk.nextToken());
-        int m = Integer.parseInt(tk.nextToken());
+        BigInteger n = new BigInteger(tk.nextToken());
+        BigInteger m = new BigInteger(tk.nextToken());
 
         System.out.println(lcm(n, m));
 
