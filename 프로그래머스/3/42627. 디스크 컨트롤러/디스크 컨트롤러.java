19dd4e672c8402fcb6f35ed6fcddf6fd7ef7c359
Meda <med4ndro@gmail.com>
2026-08-09 22:16:59 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.82 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,6 +1,31 @@
+import java.util.*;
+
 class Solution {
-    public int solution(int[][] jobs) {
-        int answer = 0;
-        return answer;
-    }
+	public int solution(int[][] jobs) {
+		List<int[]> jobsList = Arrays.asList(jobs);
+		jobsList.sort((j1, j2) -> Integer.compare(j1[0], j2[0]));
+		int retrunTimeSum = 0;
+		PriorityQueue<int[]> pq = new PriorityQueue<>((j1, j2) -> Integer.compare(j1[1], j2[1]));
+		int time = 0;
+		for (int i = 0; i < jobsList.size() || !pq.isEmpty();) {
+			if (pq.isEmpty() && jobsList.get(i)[0] >= time) {
+				time = jobsList.get(i)[0];
+			}
+
+			for (; i < jobsList.size(); i++) {
+				if (jobsList.get(i)[0] <= time) {
+					pq.offer(jobsList.get(i));
+				} else {
+					break;
+				}
+			}
+
+			int[] polledJob = pq.poll();
+			time += polledJob[1];
+			retrunTimeSum += time - polledJob[0];
+
+		}
+
+		return retrunTimeSum / jobs.length;
+	}
 }
