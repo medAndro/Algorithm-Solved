f2c206e458b524408914565c0004da396664e60a
Meda <med4ndro@gmail.com>
2026-08-26 23:17:56 +0900

    [level 3] Title: 징검다리 건너기, Time: 64.50 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,41 +1,58 @@
+//세그먼트 트리를 이용한 풀이
+
 class Solution {
-	final int MAX_STONE = 200_000_000;
+	final int MAX_STONE = 200_000;
 	int[] stones;
+	int N;
 	int k;
+
+	int tree[];
 
 	public int solution(int[] stones, int k) {
 		this.stones = stones;
+		this.N = stones.length;
 		this.k = k;
-		int left = 1;
-		int right = MAX_STONE + 1;
 
-		while (left < right) {
-			int mid = left + (right - left) / 2;
+		this.tree = new int[N * 4];
+		build(1, 0, N - 1);
+		int answer = Integer.MAX_VALUE;
 
-			if (isCanAcross(mid)) {
-				left = mid + 1;
-			} else {
-				right = mid;
-			}
-
+		for (int i = 0; i + k <= N; i++) {
+			answer = Math.min(answer, maxRange(1, 0, N - 1, i, i + k - 1));
 		}
+		
+		return answer;
 
-		return left - 1;
 	}
 
-	boolean isCanAcross(int chk) {
-		int chain = 0;
-
-		for (int stone : stones) {
-			if (stone < chk) {
-				chain++;
-				if (chain == k) {
-					return false;
-				}
-			} else {
-				chain = 0;
-			}
+	void build(int node, int start, int end) {
+		if (start == end) {
+			tree[node] = stones[start];
+			return;
 		}
-		return true;
+		int mid = (start + end) / 2;
+
+		build(node * 2, start, mid);
+		build(node * 2 + 1, mid + 1, end);
+
+		tree[node] = Math.max(tree[node * 2], tree[node * 2 + 1]);
+	}
+
+	// left, right는 요청 구간, start, end는 탐색중인 구간
+	int maxRange(int node, int start, int end, int left, int right) {
+		// 전혀 겹치지 않는 경우
+		if (end < left || right < start) {
+			return Integer.MIN_VALUE;
+		}
+
+		// 요청 구간이 탐색중인 구간 내에 완전히 포함되는 경우
+		if (left <= start && end <= right) {
+			return tree[node];
+		}
+
+		// 일부만 겹치는 경우;
+		int mid = (start + end) / 2;
+		return Math.max(maxRange(node * 2, start, mid, left, right), maxRange(node * 2 + 1, mid + 1, end, left, right));
+
 	}
 }
