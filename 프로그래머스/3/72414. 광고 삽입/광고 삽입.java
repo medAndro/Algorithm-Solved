6a0a15368a4d06dccb8d6eee48f76f2de120a330
Meda <med4ndro@gmail.com>
2026-08-27 15:28:38 +0900

    [level 3] Title: 광고 삽입, Time: 229.70 ms, Memory: 314 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -4,14 +4,17 @@ class Solution {
 
 	int playTime;
 	int advTime;
-	long imos[];
-	long sumLogs[];
+	int imos[];
+	int sums[];
+
+	long tree[];
 
 	public String solution(String play_time, String adv_time, String[] logs) {
 		this.playTime = timeStrToSec(play_time);
 		this.advTime = timeStrToSec(adv_time);
-		this.imos = new long[playTime + 1];
-		this.sumLogs = new long[playTime + 1];
+		this.imos = new int[playTime + 1];
+		this.sums = new int[playTime + 1];
+		this.tree = new long[playTime * 4];
 
 		for (String log : logs) {
 			StringTokenizer tk = new StringTokenizer(log, "-");
@@ -23,26 +26,58 @@ class Solution {
 		}
 
 		int sum = 0;
-		for (int i = 0; i < playTime + 1; i++) {
+		for (int i = 0; i <= playTime; i++) {
 			sum += imos[i];
-			sumLogs[i] = sum;
-			if (i > 0) {
-				sumLogs[i] += sumLogs[i - 1];
-			}
+			sums[i] = sum;
 		}
 
+		buildTree(1, 0, playTime - 1);
+
+		long max = 0;
 		int answer = 0;
-		long maxPrefixSum = sumLogs[advTime - 1];
-		for (int i = 1; i <= (playTime - advTime); i++) {
-			long curSum = sumLogs[i + advTime - 1] - sumLogs[i - 1];
-			if (maxPrefixSum < curSum) {
-				maxPrefixSum = curSum;
+
+		for (int i = 0; i <= playTime - advTime; i++) {
+			long rangeSum = sumRange(i, i + advTime - 1);
+			if (max < rangeSum) {
 				answer = i;
+				max = rangeSum;
+
 			}
 
 		}
 
 		return secToTimeStr(answer);
+	}
+
+	void buildTree(int node, int start, int end) {
+		if (start == end) {
+			tree[node] = sums[start];
+			return;
+		}
+
+		int mid = (start + end) / 2;
+
+		buildTree(node * 2, start, mid);
+		buildTree(node * 2 + 1, mid + 1, end);
+
+		tree[node] = tree[node * 2] + tree[node * 2 + 1];
+	}
+
+	long sumRange(int start, int end) {
+		return sumRangeByTree(1, 0, playTime - 1, start, end);
+	}
+
+	long sumRangeByTree(int node, int nStart, int nEnd, int targetStart, int targetEnd) {
+		if (targetStart > nEnd || targetEnd < nStart) {
+			return 0;
+		} else if (targetStart <= nStart && nEnd <= targetEnd) {
+			return tree[node];
+		} else {
+			int nMid = (nStart + nEnd) / 2;
+			long leftSum = sumRangeByTree(node * 2, nStart, nMid, targetStart, targetEnd);
+			long rightSum = sumRangeByTree(node * 2 + 1, nMid + 1, nEnd, targetStart, targetEnd);
+			return leftSum + rightSum;
+		}
 	}
 
 	int timeStrToSec(String time) {
