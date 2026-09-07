7261a06aad447ed7b5bbd2ece8226c838b44ceca
Meda <med4ndro@gmail.com>
2026-09-07 13:43:07 +0900

    [D3] Title: 햄버거 다이어트, Time: 729 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-//같은 재료를 여러번 사용할 수 없으므로 0/1 kanpsack 문제
+//조합 완탐 풀이
 class Solution {
 	public static class 재료정보 {
 		int 맛_점수;
@@ -16,17 +16,24 @@ class Solution {
 
 	}
 
+	static int answer;
+	static int 재료의_수;
+	static int 제한_칼로리;
+	static 재료정보[] 재료들;
+	static int[] selected = new int[20];
+
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine()); // 테스트 케이스 수
 		StringBuilder sb = new StringBuilder();
 
 		for (int test_case = 1; test_case <= T; test_case++) {
+			answer = 0; // 테케별 초기화
 			StringTokenizer tk = new StringTokenizer(br.readLine());
 
-			int 재료의_수 = Integer.parseInt(tk.nextToken());
-			int 제한_칼로리 = Integer.parseInt(tk.nextToken());
-			재료정보[] 재료들 = new 재료정보[재료의_수];
+			재료의_수 = Integer.parseInt(tk.nextToken());
+			제한_칼로리 = Integer.parseInt(tk.nextToken());
+			재료들 = new 재료정보[재료의_수];
 
 			// 이번 테스트 케이스의 재료 정보들 입력받기
 			for (int idx = 0; idx < 재료의_수; idx++) {
@@ -36,20 +43,34 @@ class Solution {
 				재료들[idx] = new 재료정보(맛_점수, 칼로리);
 			}
 
-			// 인덱스 == 칼로리, 값1 == 합산 맛 점수
-			int[] dp = new int[제한_칼로리 + 1];
-
-			for (재료정보 재료 : 재료들) {
-				for (int kcal = 제한_칼로리; kcal >= 재료.칼로리; kcal--) {
-					int 기존_칼로리 = dp[kcal];
-					int 새로운_칼로리 = dp[kcal - 재료.칼로리] + 재료.맛_점수;
-					dp[kcal] = Math.max(기존_칼로리, 새로운_칼로리);
-				}
+			for (int r = 1; r <= 재료의_수; r++) {
+				combi(r, 0, 0);
 			}
 
-			int answer = dp[제한_칼로리];
 			sb.append("#").append(test_case).append(" ").append(answer).append("\n");
 		}
 		System.out.println(sb.toString());
+	}
+
+	// 모든 재료중 r개 재료의 조합(nCr)을 구하여 answer에 최대 칼로리 갱신하는 함수
+	static void combi(int r, int depth, int start) {
+		if (depth == r) {
+			int 칼로리합 = 0;
+			int 점수합 = 0;
+			for (int i = 0; i < r; i++) {
+				점수합 += 재료들[selected[i]].맛_점수;
+				칼로리합 += 재료들[selected[i]].칼로리;
+				if (칼로리합 > 제한_칼로리) {
+					return;
+				}
+			}
+			answer = Math.max(answer, 점수합);
+			return;
+		}
+
+		for (int i = start; i < 재료의_수; i++) {
+			selected[depth] = i;
+			combi(r, depth + 1, i + 1);
+		}
 	}
 }
