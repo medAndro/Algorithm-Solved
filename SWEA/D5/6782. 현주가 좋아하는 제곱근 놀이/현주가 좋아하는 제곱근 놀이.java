322bba6440ce0a20fba80b83bca65bd36b84c47b
Meda <med4ndro@gmail.com>
2026-09-18 09:39:18 +0900

    [D5] Title: 현주가 좋아하는 제곱근 놀이, Time: 129 ms, Memory: 41,320 KB -BaekjoonHub

---
+++ SWEA/D5/6782. 현주가 좋아하는 제곱근 놀이/현주가 좋아하는 제곱근 놀이.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+class Solution {
+	static long pows[] = new long[1000001];
+	int topWriteIdx = 0;
+
+	public static void main(String args[]) throws Exception {
+		for (long i = 1; i <= 1000000; i++) {
+			pows[(int) i] = i * i;
+		}
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			long N = Long.parseLong(br.readLine());
+
+			long answer = 0;
+			while (N != 2L) {
+				long i = (long) Math.sqrt(N);
+				for (; i <= 1000000; i++) {
+					long pow = pows[(int) i];
+					if (pow >= N) {
+						answer += pow - N + 1;
+						N = (int) i;
+						break;
+					}
+				}
+			}
+			sb.append("#" + test_case + " " + answer + "\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
