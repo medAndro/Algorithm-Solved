6a39f6c5b5b4481f99f19286eec74d1818afd228
Meda <med4ndro@gmail.com>
2026-09-20 22:35:04 +0900

    [Passed] 재귀함수의 꽃 (137ms, 10MB)

---
+++ Codetree/trail2/재귀함수의 꽃/the-flower-of-the-recursive-function.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        recursive(n);
+    }
+    static void recursive(int n){
+        if(n == 0){
+            return;
+        }
+        System.out.print(n + " ");
+        recursive(n-1);
+        System.out.print(n + " ");
+    }
+}
