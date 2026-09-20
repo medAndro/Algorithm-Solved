94e68fbebdd4d5fc9a2ce1441e5f93244e2c9b6c
Meda <med4ndro@gmail.com>
2026-09-20 22:38:23 +0900

    [Passed] 재귀함수를 이용한 별 출력 2 (236ms, 13MB)

---
+++ Codetree/trail2/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        recursiveStar(n);
+    }
+
+    static void recursiveStar(int n){
+        if(n == 0){
+            return;
+        }
+        printSingleStarLine(n);
+        recursiveStar(n-1);
+        printSingleStarLine(n);
+    }
+    static void printSingleStarLine(int n){
+        for(int i =0; i<n; i++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+}
